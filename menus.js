03b2fef2f650157f4d0e04b46a96f446950c3b62
Joan Su Nombre <joansburgos@gmail.com>
2025-10-09 21:01:47 -0300

    feat: Implementación de la lógica de visibilidad del botón de cierre 'X' y refactorización de la barra de navegación (desktop/mobile).

--- menus.js
+++ menus.js
@@ -4,11 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuPrincipal = document.getElementById('menu-principal');
     const btonBuscador = document.getElementById('bton-buscador-mobile');
     const buscadorDesplegado = document.getElementById('buscador-desplegado');
+    const btnMenuH = document.getElementById('cierre-mh');
+
 
     // Abrir/cerrar menú hamburguesa
     btonMenu.addEventListener('click', () => {
-        menuPrincipal.classList.toggle('activo');
+        menuPrincipal.classList.add('activo');
+        btonMenu.classList.add('elemento-oculto');
+        btnMenuH.classList.add('activo');
     });
+
+    btnMenuH.addEventListener('click', ()=> {
+        menuPrincipal.classList.remove('activo');
+        btonMenu.classList.remove('elemento-oculto');
+        btnMenuH.classList.remove('activo');
+    })
 
     // Abrir/cerrar buscador móvil
     btonBuscador.addEventListener('click', () => {
