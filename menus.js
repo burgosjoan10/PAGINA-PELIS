9835a64ccff9d8ecbfc9403480e609c775af112d
Joan Su Nombre <joansburgos@gmail.com>
2025-10-10 02:47:12 -0300

    feat: Implementación de la lógica de visibilidad del botón de cierre 'X' y refactorización de la barra de navegación (desktop/mobile) TERMINADAS.

--- menus.js
+++ menus.js
@@ -4,7 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuPrincipal = document.getElementById('menu-principal');
     const btonBuscador = document.getElementById('bton-buscador-mobile');
     const buscadorDesplegado = document.getElementById('buscador-desplegado');
+    const buscador = document.getElementById('buscador');
     const btnMenuH = document.getElementById('cierre-mh');
+    const btnMenuB = document.getElementById('cierre-b');
+    
 
 
     // Abrir/cerrar menú hamburguesa
@@ -14,14 +17,21 @@ document.addEventListener('DOMContentLoaded', () => {
         btnMenuH.classList.add('activo');
     });
 
-    btnMenuH.addEventListener('click', ()=> {
+    btnMenuH.addEventListener('click', () => {
         menuPrincipal.classList.remove('activo');
         btonMenu.classList.remove('elemento-oculto');
         btnMenuH.classList.remove('activo');
-    })
+    });
 
     // Abrir/cerrar buscador móvil
     btonBuscador.addEventListener('click', () => {
         buscadorDesplegado.classList.toggle('activo');
+        btnMenuB.classList.add('activo');
+        buscador.classList.add('activo');
+    });
+
+    btnMenuB.addEventListener('click', () => {
+        buscadorDesplegado.classList.remove('activo');
+        btnMenuB.classList.remove('activo');
     });
 });
